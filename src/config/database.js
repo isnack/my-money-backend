e29a44c4559e50ffa18f1db30c90bb2a695c59b6
isnack <isnack_yagami@hotmail.com>
2020-03-13 03:09:58 -0300

    fix warning message warning message

--- src/config/database.js
+++ src/config/database.js
@@ -3,5 +3,5 @@ mongoose.promise = global.promise;
 
 module.exports = mongoose.connect(
   `mongodb+srv://${process.env.DATABASE_USER}:${process.env.DATABASE_PASSWORD}@todo-elddu.mongodb.net/test?retryWrites=true&w=majority`,
-  { useNewUrlParser: true }
+  { useNewUrlParser: true, useUnifiedTopology: true }
 );
