406637f22b3ff1c903339bce3ef7cdf971754e89
isnack <isnack_yagami@hotmail.com>
2020-06-08 08:27:37 -0300

    serialize user returned from db for json object

--- src/api/user/authService.js
+++ src/api/user/authService.js
@@ -20,12 +20,12 @@ const login = (req, res, next) => {
     if (err) {
       return sendErrorsFromDB(res, err);
     } else if (user && bcrypt.compareSync(password, user.password)) {
-      const token = jwt.sign(user, process.env.AUTH_SECRET, {
+      const token = jwt.sign(user.toJSON(), process.env.AUTH_SECRET, {
         expiresIn: "1 day",
       });
 
       const { name, email } = user;
-      res.send({ name, email, token });
+      res.json({ name, email, token });
     } else {
       return res.status(400).send({ errors: ["Usuario/Senha inválidos!!"] });
     }
