147644405bf713810498b4aa7b26b4c066f57963
isnack <isnack_yagami@hotmail.com>
2020-06-08 07:01:24 -0300

    Add Auth Authorization

---
+++ src/config/auth.js
@@ -0,0 +1,25 @@
+const jwt = require("jsonwebtoken");
+const AUTH_SECRET = require("../../.env");
+
+module.exports = (req, res, next) => {
+  // CORS preflight request
+  if (req.method === "OPTIONS") {
+    next();
+  } else {
+    const token =
+      req.body.token || req.query.token || req.headers["authorization"];
+    if (!token) {
+      return res.status(403).send({ errors: ["No token provided."] });
+    }
+    jwt.verify(token, AUTH_SECRET, function (err, decoded) {
+      if (err) {
+        return res.status(403).send({
+          errors: ["Failed to authenticate token."],
+        });
+      } else {
+        req.decoded = decoded;
+        next();
+      }
+    });
+  }
+};
--- src/config/cors.js
+++ src/config/cors.js
@@ -1,9 +1,9 @@
-module.exports = function(req, res, next) {
+module.exports = function (req, res, next) {
   res.header("Access-Control-Allow-Origin", "*");
   res.header("Access-Control-Allow-Methods", "GET, POST, DELETE, PUT");
   res.header(
     "Access-Control-Allow-Headers",
-    "Origin, X-Requested-With, Content-Type, Accept"
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
   );
   next();
 };
